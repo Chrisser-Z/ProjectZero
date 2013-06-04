1b47f68d40352d899c720075ae916ed010b69a55
hwiederk <hwiederk@gmail.com>
2013-06-04 02:11:29 +0200

    .

--- src/network/DataOUT.java
+++ src/network/DataOUT.java
@@ -18,25 +18,31 @@ public class DataOUT {
 	}
 
 	private DatagramSocket udpSocket = null;
-	private InetSocketAddress inetSocket = new InetSocketAddress("127.0.0.1", 52012);
+	private InetSocketAddress inetSocket = null;
 	private DatagramPacket datagram = null;
 
-	public void buildDataPacket(byte[] payload) {
+	public void buildDataPacket(String payloadString) {
+
+	}
+	
+	public void sendData(String payloadString) {
+		byte[] payload = payloadString.getBytes();
 		try {
 			datagram = new DatagramPacket(payload, payload.length, inetSocket);
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public void sendData() {
 		try {
 			udpSocket.send(datagram);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void setInetsocket(String ip) {
+		inetSocket = new InetSocketAddress(ip, 52012);
 	}
 
 }
