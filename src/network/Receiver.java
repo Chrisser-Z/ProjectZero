b1dc3659e3b9f238f1c5f5d9ee2000bb6759a0a1
hwiederk <hwiederk@gmail.com>
2013-06-09 21:30:32 +0200

    .

--- src/network/Receiver.java
+++ src/network/Receiver.java
@@ -5,41 +5,36 @@ import gameContent.Enemy;
 
 import java.util.HashMap;
 
-
 import core.DataIN;
 
-
-
 public class Receiver {
-	
-	
+
 	private DataIN dataIn;
 	private DataTranslater dataTranslater;
-	
-	
+
 	public Receiver() {
 		dataIn = new DataIN();
 		dataIn.start();
 		dataTranslater = new DataTranslater();
 	}
-	
-	public HashMap<Integer, Byte> getData(){
-		if(!compareSecNum()) {
-			HashMap<Integer, Byte> gameLogic = dataTranslater.translateData(dataIn.getNewData());	
+
+	public HashMap<Integer, Byte> getData() {
+		if (!compareSeqNum()) {
+			HashMap<Integer, Byte> gameLogic = dataTranslater.translateData(dataIn.getNewData());
 			Definitions.emitPermission = true;
-			return gameLogic; 
+			return gameLogic;
 		} else {
 			return null;
-		}		 
+		}
 	}
-	
-	private Boolean compareSecNum() {
+
+	private Boolean compareSeqNum() {
 		if (dataIn.getIncomingSeqNum() == Definitions.seqNum) {
 			return true;
-		}	
+		}
 		Definitions.seqNum = dataIn.getIncomingSeqNum();
 		System.out.println(Definitions.seqNum);
-		return false;		
+		return false;
 	}
 
 }
